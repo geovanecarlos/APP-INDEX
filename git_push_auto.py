bfce90d8b5896a6c435af687f291f8255017f723
geovanecarlos <geovanecarlos.miguel@gmail.com>
2025-07-11 15:08:11 -0300

    Atualização em 2025-07-11 15:08:11

--- git_push_auto.py
+++ git_push_auto.py
@@ -2,7 +2,7 @@ import os
 import datetime
 
 # Mensagem de commit com data e hora
-mensagem = f"Atualização automática em {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+mensagem = f"Atualização em {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 
 # Comandos Git
 os.system("git add .")
